882855da4316ef7d1f489fd2b624baad8c157d5f
Jakub Kucharczyk <kubak333@gmail.com>
2023-05-28 15:20:35 +0200

    Dalsze konfigurowanie API

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID Oh14DF0gfckhW7DkFxBbvYI0KdxwLRqnoNErvGfXdpQ'
         },
         params: {
-            query: 'cars'
+            query: term,
         }
     });
 
-    console.log(response);
-    return response;
+    return response.data.results;
 }
 
 export default searchImages;

029aa5f6d053466e8e765fc6e67996cf1696e445
Jakub Kucharczyk <kubak333@gmail.com>
2023-06-14 20:55:40 +0200

    Aplikacja książkowa skończona - po refaktoryzacji

--- src/App.js
+++ src/App.js
@@ -1,73 +1,20 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import BookCreate from "./components/BookCreate";
+import { useEffect, useContext } from 'react';
+import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './context/books';
 
 function App() {
-    const [books, setBooks] = useState([]);
-    
-    const fetchBooks = async () => {
-        const response = await axios.get('http://localhost:3001/books');
+  const { fetchBooks } = useContext(BooksContext);
 
-        setBooks(response.data);
-    };
-
-    useEffect(() => {
-        fetchBooks();
-    }, []);
-
-    // DON'T DO THIS:
-    // fetchBooks(); // TO POWODUJE INFINITY LOOPA - API jest pobierane non stop! DO TEGO UZYWAMY useEffect!
-
-    const editBookById = async (id, newTitle) => {
-
-        const response = await axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle
-        });
-
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return {...book, ...response.data} // to oznacza -> lewy argument tutaj dodajemy, prawy argument, co dodajemy - dodajemy wszystkie rzeczy z odpowiedzi, które się róznią
-            }
-
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    }
-
-
-
-
-    const deleteBookById = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-
-    setBooks(updatedBooks);
-    }
-
-
-
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books', {
-        title
-    });
-
-    const updatedBooks = [
-      ...books,
-      response.data
-    ];
-    setBooks(updatedBooks);
-  };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <div className="app">
-        <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
-      <BookCreate onCreate={createBook} />
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from './context/books';
@@ -8,7 +8,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-<Provider>
+  <Provider>
     <App />
-</Provider>
-)
+  </Provider>
+);
